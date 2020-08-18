0c7f634eed07c5e6d8ed17ee5444f79716234be2
Tal <talbn1@gmail.com>
2020-08-18 16:33:01 +0300

    first commit

---
+++ src/main/java/com/talbn1/bank_web/BankWebApplication.java
@@ -0,0 +1,13 @@
+package com.talbn1.bank_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankWebApplication.class, args);
+    }
+
+}
